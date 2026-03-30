643769c3654a02badbe2162abd2139a0ba4dfc60
Crypoune <a.messenet@gmail.com>
2026-03-30 19:56:02 +0200

    fix: move auth on the top

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -41,6 +41,9 @@ def create_app(config_class="config.DevelopmentConfig"):
     # Enregistrement des namespaces (routes) de chaque ressource
     # Chaque namespace regroupe les endpoints liés à une entité métier
 
+    from app.api.v1.auth import api as auth_ns
+    api.add_namespace(auth_ns, path='/api/v1/auth')             # Authentification
+
     from app.api.v1.users import api as users_ns
     api.add_namespace(users_ns, path='/api/v1/users')           # Gestion des utilisateurs
 
@@ -52,8 +55,5 @@ def create_app(config_class="config.DevelopmentConfig"):
 
     from app.api.v1.reviews import api as reviews_ns
     api.add_namespace(reviews_ns,   path='/api/v1/reviews')     # Gestion des avis
-
-    from app.api.v1.auth import api as auth_ns
-    api.add_namespace(auth_ns, path='/api/v1/auth')  # Authentification
 
     return app  # Retourne l'instance de l'application configurée
